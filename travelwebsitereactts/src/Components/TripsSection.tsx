be6c702ac64209755dab6117df3680ed2c4d532d
Mohammad Bashirinia <mohammadbashirinia777@gmail.com>
2023-07-14 20:53:25 +0330

    added trip id to trip

--- travelwebsitereactts/src/Components/TripsSection.tsx
+++ travelwebsitereactts/src/Components/TripsSection.tsx
@@ -7,6 +7,8 @@ const TripsSection = () => {
         <Trip travelId={3} travelClass="First Class" duration="0h 50m" from="Khark Island, Iran" to="Shiraz, Iran" price={79} />
         <Trip travelId={4} travelClass="First Class" duration="1h 10m" from="Bushehr, Iran" to="Shiraz, Iran" price={100} />
         <Trip travelId={5} travelClass="Business Class" duration="1h 10m" from="Bushehr, Iran" to="Shiraz, Iran" price={100} />
+        <Trip travelId={6} travelClass="Business Class" duration="1h 10m" from="Bushehr, Iran" to="Shiraz, Iran" price={100} />
+        <Trip travelId={7} travelClass="Business Class" duration="1h 10m" from="Bushehr, Iran" to="Shiraz, Iran" price={100} />
        
     </div>
 };
