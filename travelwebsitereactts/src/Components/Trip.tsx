6fd668c6e0861173e6b80c0f0e33048bbb7224e1
Mohammad Bashirinia <mohammadbashirinia777@gmail.com>
2023-07-14 20:32:30 +0330

    Parametrized the trip component

--- travelwebsitereactts/src/Components/Trip.tsx
+++ travelwebsitereactts/src/Components/Trip.tsx
@@ -3,21 +3,28 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import RoundLinkButton from "./RoundLinkButton";
 import "./styles/Trip.css";
 
-const Trip = () => {
+const Trip = (props: {
+    price: number,
+    from: string,
+    to: string,
+    travelClass: string,
+    duration: string,
+    travelId: number
+}) => {
     return <div className="flex flex-col p-[50px] py-[80px] gap-10">
         <div className="flex justify-between">
             <div className="flex items-center gap-3">
                 <FontAwesomeIcon icon={faPlane} />
-                <span>First Class</span>
+                <span>{ props.travelClass }</span>
             </div>
 
             <div className="">
-                <span className="glow-text">994.99$</span>
+                <span className="glow-text">{ props.price }$</span>
             </div>
         </div>
 
         <div>
-            <p><span className="text-main-yellow">Manchester, UK</span> to <span className="text-main-yellow">Madrid, Spain</span> </p>
+            <p><span className="text-main-yellow">{props.from}</span> to <span className="text-main-yellow">{ props.to }</span> </p>
         </div>
 
         <div className="flex items-center">
@@ -26,12 +33,12 @@ const Trip = () => {
                 <FontAwesomeIcon fontSize={10} icon={faLocationDot} />
             </div>
             <div className="mb-1 ms-3"><p className="text-[20px] tracking-[15px]">..........</p></div>
-            <div className="ms-3 mt-1"><p className="text-[14px]">12h 32m</p></div>
+            <div className="ms-3 mt-1"><p className="text-[14px]">{ props.duration }</p></div>
         </div>
 
         <div className="flex gap-5 justify-between">
-            <RoundLinkButton name="Read More"  url="a"/>
-            <RoundLinkButton name="Buy" glowing={true}  url="a"/>
+            <RoundLinkButton name="Read More" url={"TicketDetails?id=" + props.travelId} />
+            <RoundLinkButton name="Buy" glowing={true} url={"BuyTicket?id=" + props.travelId } />
         </div>
     </div>
 };
--- travelwebsitereactts/src/Components/TripsSection.tsx
+++ travelwebsitereactts/src/Components/TripsSection.tsx
@@ -2,15 +2,12 @@ import Trip from "./Trip";
 
 const TripsSection = () => {
     return <div className="owl-carousel owl-theme">
-        <Trip />
-        <Trip />
-        <Trip />
-        <Trip />
-        <Trip />
-        <Trip />
-        <Trip />
-        <Trip />
-        <Trip />
+        <Trip travelId={1} travelClass="Economy Class" duration="1h 10m" from="Bushehr, Iran" to="Shiraz, Iran" price={100} />
+        <Trip travelId={2} travelClass="Business Class" duration="2h 10m" from="Bushehr, Iran" to="Tehran, Iran" price={249} />
+        <Trip travelId={3} travelClass="First Class" duration="0h 50m" from="Khark Island, Iran" to="Shiraz, Iran" price={79} />
+        <Trip travelId={4} travelClass="First Class" duration="1h 10m" from="Bushehr, Iran" to="Shiraz, Iran" price={100} />
+        <Trip travelId={5} travelClass="Business Class" duration="1h 10m" from="Bushehr, Iran" to="Shiraz, Iran" price={100} />
+       
     </div>
 };
 
